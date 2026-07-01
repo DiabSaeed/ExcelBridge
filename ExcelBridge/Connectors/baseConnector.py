01cf0e123d382e3043aecc354bbdfc1fa19f15d8
Diab_Saeed <diabsaeed7@gmail.com>
2026-07-01 05:30:23 +0300

    Adding baseConnector interface

---
+++ ExcelBridge/Connectors/baseConnector.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Literal
+from pandas import DataFrame
+
+
+class BaseConnector(ABC):
+    @abstractmethod
+    def connect(self, **kwargs) -> Any:
+        pass
+    
+    def disconnect(self) -> None:
+        pass
+    
+    @abstractmethod
+    def execute_query(self, query: str, warehouse_id: str, **kwargs) -> Dict[str, Any]:
+        pass
+    
+    @abstractmethod
+    def run_job(self,job_id:int, **kwargs) -> Any:
+        pass
+    
+    @abstractmethod
+    def run_notebook(self, notebook_path: str, cluster_id: str, **kwargs) -> Any:
+        pass
+    
+    @abstractmethod
+    def upload_df(self, df: DataFrame, table_name: str, mode: Literal['fail', 'replace', 'append', 'delete_rows'] = 'append', **kwargs) -> bool:
+        pass
+    
+    @abstractmethod
+    def download_df(self, table_name: str, query: str, **kwargs) -> DataFrame:
+        pass    
