fc40364d042f73fe498fd3d1ccad6be9517fbc86
Diab_Saeed <diabsaeed7@gmail.com>
2026-07-03 22:00:00 +0300

    adding the rest endpoints

--- ExcelBridge/api/routes/data.py
+++ ExcelBridge/api/routes/data.py
@@ -1,7 +1,9 @@
 from fastapi import Request, APIRouter, HTTPException
 from helper import ResponseEnums
-from models.RequestModels import QueryRequest
+from models.RequestModels import QueryRequest, JobRequest, NotebookRequest
+from models.DataFrameModel import UploadDataFrameRequest, DownloadDataFrameRequest
 from Controllers.ConnectorController import ConnectorController
+from Controllers.DataController import DataController
 
 get_connector = ConnectorController("databricks")
 
@@ -29,7 +31,6 @@ async def query(
 ):
     try:
         connector = await get_connector(request, req.config)
-        # connector = connector.connect(req.config.host,req.config.token)
         result = connector.execute_query(
             query=req.query,
             warehouse_id=req.warehouse_id
@@ -41,3 +42,84 @@ async def query(
         }
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
+@data_router.post("/job")
+async def run_job(
+    req: JobRequest,
+    request: Request
+):
+    try:
+        connector = await get_connector(request,req.config)
+        
+        result = connector.run_job(req.job_id)
+        
+        return {
+            "Status": ResponseEnums.CONNECTION_SUCCEEDED.value,
+            "result": result
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@data_router.post("/notebook")
+async def run_notebook(
+    req:NotebookRequest,
+    request: Request
+):
+    try:
+        connector = await get_connector(request,req.config)
+        
+        result = connector.run_notebook(req.notebook_path,req.cluster_id)
+        
+        return {
+            "Status": ResponseEnums.CONNECTION_SUCCEEDED.value,
+            "result": result
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@data_router.post("/uploadDF")
+async def upload_df(
+    req: UploadDataFrameRequest,
+    request: Request
+):
+    try:
+        processed_df = DataController().process_uploaded(response=req.data)
+        connector = await get_connector(request,req.config)
+        result = connector.upload_df(
+            df = processed_df,
+            table_name = req.table_name,
+            mode = req.mode,
+            http_path = req.config.http_path,
+            catalog=req.config.catalog,  
+            schema=req.config.schema
+        )
+        return {
+            "Status": ResponseEnums.CONNECTION_SUCCEEDED.value,
+            "result": result
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@data_router.post("/downloadDF")
+async def download_df(
+    req: DownloadDataFrameRequest,
+    request: Request
+):
+    try:
+        connector = await get_connector(request,req.config)
+        result = connector.download_df(
+            query = req.query,
+            http_path = req.config.http_path,
+            catalog=req.config.catalog,  
+            schema=req.config.schema
+        )
+        processed_response = DataController().process_downloaded(df=result)
+        return {
+            "Status": ResponseEnums.CONNECTION_SUCCEEDED.value,
+            "result": processed_response
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    
